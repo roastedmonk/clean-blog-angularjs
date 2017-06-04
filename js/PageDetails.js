809e220b57c9149a81acc903adb363238b6cd117
Omkar Subramaniam <subramaniam@engineer.com>
2017-06-04 23:01:44 +0530

    -- Modified the Layout and asthetics --

--- js/PageDetails.js
+++ js/PageDetails.js
@@ -1,4 +1,5 @@
 var PageDetails = {
+    blog_home:"#RoastedMonk",
     post:{
         page_title : "#RoastedMonk",
         page_subtitle : "A Blog from thoughts of #RoastedMonk"
@@ -10,5 +11,13 @@ var PageDetails = {
     contact : {
         page_title:'Contact Me',
 		page_subtitle: 'Have questions? I have answers (maybe).'
-    }
+    },
+    facebook_link: "https://www.facebook.com/omkarsubramaniam",
+    twitter_link: "https://twitter.com/om_speaks",
+    linkedin_link: "https://in.linkedin.com/in/subramnat",
+    email_link:"mailto:subramaniam@engineer.com?subject=#RoastedMonk",
+    profile_image: "https://scontent-fra3-1.xx.fbcdn.net/v/t1.0-9/16683807_1355348314522904_4944027990690126632_n.jpg?oh=f6d64c7b614ac4e685e0e298d68cba98&oe=599CD9A4",
+    profile_name:"Omkar Subramaniam",
+
+    version_number: 1000002
 }
--- js/app/app.js
+++ js/app/app.js
@@ -1,325 +1,268 @@
-	// CREATE THE MODULE AND NAME IT cleanBlog
-	var app = angular.module("app", ["ngAnimate", "ui.router", "ngSanitize",]);
+// CREATE THE MODULE AND NAME IT cleanBlog
+var app = angular.module("app", ["ngAnimate", "ui.router", "ngSanitize",]);
 
-	app.run(['$rootScope', '$state', '$stateParams', function ($rootScope, $state, $stateParams)
-	{
-	    $rootScope.$state = $state;
-	    $rootScope.$stateParams = $stateParams;
+app.run(['$rootScope', '$state', '$stateParams', function ($rootScope, $state, $stateParams) {
+	$rootScope.$state = $state;
+	$rootScope.$stateParams = $stateParams;
 
-			$rootScope.page = {};
-			$rootScope.page.title = PageDetails.post.page_title;
+	$rootScope.page = {};
+	$rootScope.page.blog_home = PageDetails.blog_home;
+	$rootScope.page.title = PageDetails.post.page_title;
+	$rootScope.page.facebook_link = PageDetails.facebook_link;
+	$rootScope.page.twitter_link = PageDetails.twitter_link;
+	$rootScope.page.email_link = PageDetails.email_link;
 
-			$rootScope.$on('$stateChangeSuccess', function (event, current, previous) {
-				  $rootScope.page.title = current.title;
-			});
-	}]);
-
-	// CONFIGURE OUR ROUTES
-	app.config(function($stateProvider, $urlRouterProvider, $locationProvider)
-	{
-
-		$urlRouterProvider.otherwise('/');
-
-		$stateProvider
-			.state('home', {
-				url: '/',
-				templateUrl : 'pages/home.html',
-				controller: 'mainController',
-        title: PageDetails.post.page_title
-			})
-
-			.state('about', {
-				url: '/about',
-				templateUrl : 'pages/page.html',
-				controller: 'aboutController',
-        title: PageDetails.post.page_title +' - '  + PageDetails.about.page_title
-			})
-
-			.state('contact', {
-				url: '/contact',
-				templateUrl : 'pages/contact.html',
-				controller: 'contactController',
-        title: PageDetails.post.page_title +' - '  + PageDetails.contact.page_title
-			})
-
-			.state('post', {
-				url: '/post/{id}/{slug}',
-				templateUrl: 'pages/post.html',
-				controller: 'postController',
-        title: PageDetails.post.page_title
-			})
-
-			.state('pager', {
-				url: '/page/{id}',
-				templateUrl : 'pages/home.html',
-				controller  : 'mainController',
-        title: PageDetails.post.page_title
-			})
-
-		$locationProvider.html5Mode(true);
+	$rootScope.$on('$stateChangeSuccess', function (event, current, previous) {
+		$rootScope.page.title = current.title;
 	});
+}]);
 
-	// Posts Pagination
-	app.directive('postsPagination', function(){
-	   return{
-	      restrict: 'E',
-	      template: '<ul class="pager" ng-show="pager">'+
-				'<li class="previous" ng-show="currentPage != 1">'+
-					'<a ui-sref="pager({id:currentPage-1})">&larr; Newer Posts</a>'+
-				'</li>'+
-				'<li class="next" ng-show="currentPage != totalPages">'+
-					'<a ui-sref="pager({id:currentPage+1})">Older Posts &rarr;</a>'+
-				'</li>'+
-			'</ul>'
-	   };
-	});
+// CONFIGURE OUR ROUTES
+app.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
 
-	// Limit the Posts content text on the Post list page/home page
-	app.filter('limitHtml', function() {
-	    return function(text, limit) {
+	$urlRouterProvider.otherwise('/');
 
-	        var changedString = String(text).replace(/<[^>]+>/gm, '');
-	        var length = changedString.length;
+	$stateProvider
+		.state('home', {
+			url: '/',
+			templateUrl: 'pages/home.html?v='+PageDetails.version_number,
+			controller: 'mainController',
+			title: PageDetails.post.page_title
+		})
 
-	        return changedString.length > limit ? changedString.substr(0, limit - 1) + '...' : changedString;
-	    }
-	});
+		.state('about', {
+			url: '/about',
+			templateUrl: 'pages/page.html?v='+PageDetails.version_number,
+			controller: 'aboutController',
+			title: PageDetails.post.page_title + ' - ' + PageDetails.about.page_title
+		})
 
-	// Get current year
-	app.filter('currentYear',['$filter',  function($filter) {
-    	return function() {
-        	return $filter('date')(new Date(), 'yyyy');
-    	};
-	}])
+		.state('post', {
+			url: '/post/{id}/{slug}',
+			templateUrl: 'pages/post.html?v='+PageDetails.version_number,
+			controller: 'postController',
+			title: PageDetails.post.page_title
+		})
 
-	// MAIN CONTROLLER/HOME
-	app.controller('mainController', function($scope, $http, $sce, $timeout, $stateParams)
-	{
+		.state('pager', {
+			url: '/page/{id}',
+			templateUrl: 'pages/home.html?v='+PageDetails.version_number,
+			controller: 'mainController',
+			title: PageDetails.post.page_title
+		})
 
-	  $scope.page_title = PageDetails.post.page_title;
-		$scope.page_subtitle = PageDetails.post.page_subtitle;
+	$locationProvider.html5Mode(true);
+});
 
-		$scope.date = new Date();
+// Posts Pagination
+app.directive('postsPagination', function () {
+	return {
+		restrict: 'E',
+		template: '<ul class="pager" ng-show="pager">' +
+		'<li class="previous" ng-show="currentPage != 1">' +
+		'<a ui-sref="pager({id:currentPage-1})">&larr; Newer Posts</a>' +
+		'</li>' +
+		'<li class="next" ng-show="currentPage != totalPages">' +
+		'<a ui-sref="pager({id:currentPage+1})">Older Posts &rarr;</a>' +
+		'</li>' +
+		'</ul>'
+	};
+});
 
-		$scope.posts = [];
-		$scope.totalPages = 0;
-		$scope.currentPage = 1;
-		$scope.range = [];
-		$scope.loading = false;
-		$scope.pager = false;
+// Limit the Posts content text on the Post list page/home page
+app.filter('limitHtml', function () {
+	return function (text, limit) {
 
-		// DATE FORMAT
-		$scope.dateFormat = function(dateString)
-		{
-			if (dateString) {
-				var properlyFormattedDate = dateString.split(" ").join("T");
-				return new Date(properlyFormattedDate);
-			} else {
-				return null;
-			}
-		};
+		var changedString = String(text).replace(/<[^>]+>/gm, '');
+		var length = changedString.length;
 
-		// GET POSTS FUNCTION
-		$scope.getPosts = function(pageNumber) {
+		return changedString.length > limit ? changedString.substr(0, limit - 1) + '...' : changedString;
+	}
+});
 
-			pageNumber = $stateParams.id;
+// Get current year
+app.filter('currentYear', ['$filter', function ($filter) {
+	return function () {
+		return $filter('date')(new Date(), 'yyyy');
+	};
+}])
 
-			if(pageNumber===undefined)
-			{
-				pageNumber = '1';
-			}
+// MAIN CONTROLLER/HOME
+app.controller('mainController', function ($scope, $http, $sce, $timeout, $stateParams) {
+	$scope.page_title = PageDetails.post.page_title;
+	$scope.page_subtitle = PageDetails.post.page_subtitle;
 
-			$scope.loading = true;
+	$scope.date = new Date();
 
-			$http.get('json/posts-page-'+pageNumber+'.json').success(function(response) {
-				$scope.posts        = response.data;
-      			$scope.totalPages   = response.last_page;
-      			$scope.currentPage  = response.current_page;
+	$scope.posts = [];
+	$scope.totalPages = 0;
+	$scope.currentPage = 1;
+	$scope.range = [];
+	$scope.loading = false;
+	$scope.pager = false;
+
+	// DATE FORMAT
+	$scope.dateFormat = function (dateString) {
+		if (dateString) {
+			var properlyFormattedDate = dateString.split(" ").join("T");
+			return new Date(properlyFormattedDate);
+		} else {
+			return null;
+		}
+	};
+
+	// GET POSTS FUNCTION
+	$scope.getPosts = function (pageNumber) {
+
+		pageNumber = $stateParams.id;
+
+		if (pageNumber === undefined) {
+			pageNumber = '1';
+		}
+
+		$scope.loading = true;
+
+		$http.get('json/posts-page-' + pageNumber + '.json').success(function (response) {
+			$scope.posts = response.data;
+			$scope.totalPages = response.last_page;
+			$scope.currentPage = response.current_page;
+			$scope.loading = false;
+			$scope.pager = true;
+
+
+			if (pageNumber > $scope.totalPages) {
+				$scope.noResult = true;
 				$scope.loading = false;
-				$scope.pager = true;
-
-
-				if(pageNumber > $scope.totalPages)
-				{
-					$scope.noResult = true;
-					$scope.loading = false;
-					$scope.pager = false;
-				}
-			});
-		};
-
-		$scope.getPosts();
-	});
-
-	// POST CONTROLLER
-	app.controller('postController', function($scope, $http, $state, $stateParams)
-	{
-		$scope.page_title = PageDetails.about.page_title;
-
-		if($stateParams.id == undefined) {
-			$state.go('home');
-		}
-
-		// DATE FORMAT
-		$scope.dateFormat = function(dateString)
-		{
-			if (dateString) {
-				var properlyFormattedDate = dateString.split(" ").join("T");
-				return new Date(properlyFormattedDate);
-			} else {
-				return null;
+				$scope.pager = false;
 			}
-		};
-
-		post_id = $stateParams.id;
-
-		// $http.get('http://laravel.dev/api/post/'+post_id).success(function(response) {
-		$http.get('json/post.json').success(function(response) {
-			$scope.post_id = response.id;
-			$scope.post_date = response.post_date;
-			$scope.post_title = response.post_title;
-			$scope.post_author = response.author.firstname + ' ' + response.author.lastname;
-			$scope.post_content = response.post_content;
 		});
-	});
+	};
 
-	// ABOUT CONTROLLER
-	app.controller('aboutController', function($scope,$http)
-	{
-		$scope.page_title = PageDetails.about.page_title;
-		$scope.page_subtitle = PageDetails.about.page_subtitle;
-	});
+	$scope.getPosts();
+});
 
-	// Show errors on the Contact page
-	app.directive('showErrors', function ($timeout, showErrorsConfig) {
-		var getShowSuccess, linkFn;
+// POST CONTROLLER
+app.controller('postController', function ($scope, $http, $state, $stateParams) {
+	$scope.page_title = PageDetails.about.page_title;
 
-		getShowSuccess = function (options) {
-			var showSuccess;
-			showSuccess = showErrorsConfig.showSuccess;
-			if (options && options.showSuccess != null) {
-			  showSuccess = options.showSuccess;
-			}
-			return showSuccess;
-		};
+	if ($stateParams.id == undefined) {
+		$state.go('home');
+	}
 
-		linkFn = function (scope, el, attrs, formCtrl) {
-			var blurred, inputEl, inputName, inputNgEl, options, showSuccess, toggleClasses;
-
-			blurred = false;
-			options = scope.$eval(attrs.showErrors);
-			showSuccess = getShowSuccess(options);
-			inputEl = el[0].querySelector('[name]');
-			inputNgEl = angular.element(inputEl);
-			inputName = inputNgEl.attr('name');
-			if (!inputName) {
-			  throw 'show-errors element has no child input elements with a \'name\' attribute';
-			}
-
-			inputNgEl.bind('blur', function () {
-			  blurred = true;
-			  return toggleClasses(formCtrl[inputName].$invalid);
-			});
-
-			scope.$watch(function () {
-			  return formCtrl[inputName] && formCtrl[inputName].$invalid;
-			}, function (invalid) {
-			  if (!blurred) {
-			    return;
-			  }
-			  return toggleClasses(invalid);
-			});
-
-			scope.$on('show-errors-check-validity', function () {
-			  return toggleClasses(formCtrl[inputName].$invalid);
-			});
-
-			scope.$on('show-errors-reset', function () {
-			  return $timeout(function () {
-			    el.removeClass('has-error');
-			    el.removeClass('has-success');
-			    return blurred = false;
-			  }, 0, false);
-			});
-
-			return toggleClasses = function (invalid) {
-			  el.toggleClass('has-error', invalid);
-			  if (showSuccess) {
-			    return el.toggleClass('has-success', !invalid);
-			  }
-			};
-		};
-
-		return {
-			restrict: 'A',
-			require: '^form',
-			compile: function (elem, attrs) {
-			  if (!elem.hasClass('form-group')) {
-			    throw 'show-errors element does not have the \'form-group\' class';
-			  }
-			  return linkFn;
-			}
-		};
-	});
-
-	app.provider('showErrorsConfig', function () {
-	    var _showSuccess;
-	    _showSuccess = false;
-	    this.showSuccess = function (showSuccess) {
-	      return _showSuccess = showSuccess;
-	    };
-	    this.$get = function () {
-	      return { showSuccess: _showSuccess };
-	    };
-	  });
-
-	app.config(['showErrorsConfigProvider', function(showErrorsConfigProvider) {
-	  showErrorsConfigProvider.showSuccess(true);
-	}]);
-
-	// CONTACT CONTROLLER
-	app.controller('contactController', function($scope, $http)
-	{
-		$scope.page_title = PageDetails.contact.page_title;
-		$scope.page_subtitle = PageDetails.contact.page_subtitle;
-
-		$scope.phone_number = /^[0-9]+$/;
-		$scope.email = /^[a-z0-9!#$%&'*+/=?^_`{|}~.-]+@[a-z0-9-]+(\.[a-z0-9-]+)*$/i;
-
-		$scope.resultMessage;
-		$scope.formData;
-		$scope.loading = false;
-		$scope.alert = false;
-
-		// Contact on submit
-		$scope.submit = function(contactform) {
-			$scope.$broadcast('show-errors-check-validity');
-
-			if ($scope.contactForm.$valid) {
-				$scope.loading = true;
-				$http({
-					method: 'POST',
-					url: 'mail/contact_me.php',
-					data    : $.param($scope.formData),
-					headers : { 'Content-Type': 'application/x-www-form-urlencoded' }
-				}).success(function(data){
-					if(data.success) {
-						$scope.resultMessage = data.message;
-						$scope.alertClass = 'alert alert-success';
-					} else {
-						$scope.resultMessage = data.message;
-						$scope.alertClass = 'alert alert-danger';
-					}
-					$scope.loading = false;
-					$scope.alert = true;
-					$scope.reset();
-				});
-			}
-		};
-
-		// Contact on reset
-		$scope.reset = function() {
-			$scope.$broadcast('show-errors-reset');
-			$scope.formData = {};
+	// DATE FORMAT
+	$scope.dateFormat = function (dateString) {
+		if (dateString) {
+			var properlyFormattedDate = dateString.split(" ").join("T");
+			return new Date(properlyFormattedDate);
+		} else {
+			return null;
 		}
+	};
+
+	post_id = $stateParams.id;
+
+	// $http.get('http://laravel.dev/api/post/'+post_id).success(function(response) {
+	$http.get('json/post.json').success(function (response) {
+		$scope.post_id = response.id;
+		$scope.post_date = response.post_date;
+		$scope.post_title = response.post_title;
+		$scope.post_author = response.author.firstname + ' ' + response.author.lastname;
+		$scope.post_content = response.post_content;
 	});
+});
+
+// ABOUT CONTROLLER
+app.controller('aboutController', function ($scope, $http) {
+	$scope.page_title = PageDetails.about.page_title;
+	$scope.page_subtitle = PageDetails.about.page_subtitle;
+	$scope.profile_image = PageDetails.profile_image;
+	$scope.profile_name = PageDetails.profile_name;
+});
+
+// Show errors on the Contact page
+app.directive('showErrors', function ($timeout, showErrorsConfig) {
+	var getShowSuccess, linkFn;
+
+	getShowSuccess = function (options) {
+		var showSuccess;
+		showSuccess = showErrorsConfig.showSuccess;
+		if (options && options.showSuccess != null) {
+			showSuccess = options.showSuccess;
+		}
+		return showSuccess;
+	};
+
+	linkFn = function (scope, el, attrs, formCtrl) {
+		var blurred, inputEl, inputName, inputNgEl, options, showSuccess, toggleClasses;
+
+		blurred = false;
+		options = scope.$eval(attrs.showErrors);
+		showSuccess = getShowSuccess(options);
+		inputEl = el[0].querySelector('[name]');
+		inputNgEl = angular.element(inputEl);
+		inputName = inputNgEl.attr('name');
+		if (!inputName) {
+			throw 'show-errors element has no child input elements with a \'name\' attribute';
+		}
+
+		inputNgEl.bind('blur', function () {
+			blurred = true;
+			return toggleClasses(formCtrl[inputName].$invalid);
+		});
+
+		scope.$watch(function () {
+			return formCtrl[inputName] && formCtrl[inputName].$invalid;
+		}, function (invalid) {
+			if (!blurred) {
+				return;
+			}
+			return toggleClasses(invalid);
+		});
+
+		scope.$on('show-errors-check-validity', function () {
+			return toggleClasses(formCtrl[inputName].$invalid);
+		});
+
+		scope.$on('show-errors-reset', function () {
+			return $timeout(function () {
+				el.removeClass('has-error');
+				el.removeClass('has-success');
+				return blurred = false;
+			}, 0, false);
+		});
+
+		return toggleClasses = function (invalid) {
+			el.toggleClass('has-error', invalid);
+			if (showSuccess) {
+				return el.toggleClass('has-success', !invalid);
+			}
+		};
+	};
+
+	return {
+		restrict: 'A',
+		require: '^form',
+		compile: function (elem, attrs) {
+			if (!elem.hasClass('form-group')) {
+				throw 'show-errors element does not have the \'form-group\' class';
+			}
+			return linkFn;
+		}
+	};
+});
+
+app.provider('showErrorsConfig', function () {
+	var _showSuccess;
+	_showSuccess = false;
+	this.showSuccess = function (showSuccess) {
+		return _showSuccess = showSuccess;
+	};
+	this.$get = function () {
+		return { showSuccess: _showSuccess };
+	};
+});
+
+app.config(['showErrorsConfigProvider', function (showErrorsConfigProvider) {
+	showErrorsConfigProvider.showSuccess(true);
+}]);
+
